cb6e98dc6b141d90c20ce49d3447840fdcd2eb70
Elvinas Švilpa <elvinas.svilpa@mif.stud.vu.lt>
2024-09-26 18:07:52 +0300

    Finished matrix subtraction and multiplication unit tests

--- tests/MatrixUnitTest.cs
+++ tests/MatrixUnitTest.cs
@@ -121,12 +121,47 @@ namespace tests
         [Fact]
         public void OperatorMinus_CheckIfMatricesAreSubtractedCorrectly()
         {
+            Field field2 = new Field(2);
+            int[,] firstElements = { { 1, 0, 1, 1, 1 }, { 1, 0, 0, 0, 0 }, { 0, 1, 1, 1, 1 }, { 1, 1, 1, 0, 1 } };
+            Matrix firstMatrix = new Matrix(firstElements, field2.q);
+            int[,] secondElements = { { 1, 1, 0, 0, 0 }, { 0, 0, 1, 0, 0 }, { 1, 0, 1, 1, 1 }, { 0, 1, 0, 0, 0 } };
+            Matrix secondMatrix = new Matrix(secondElements, field2.q);
+            string expedtedStringOutput = "0 1 1 1 1 \n1 0 1 0 0 \n1 1 0 0 0 \n1 0 1 0 1 \n";
+            
+            Assert.Equal(expedtedStringOutput, (firstMatrix - secondMatrix).ToString());
+            
+            // ---
+            
+            Field field5 = new Field(5);
+            int[,] elements1 = { { 1, 0, 2, 3, 4 }, { 1, 1, 0, 3, 2 }, { 3, 1, 1, 2, 1 }, { 0, 1, 4, 0, 1 } };
+            int[,] elements2 = { { 1, 0, 4, 1, 3 }, { 3, 4, 1, 1, 0}, { 0, 4, 1, 1, 0 }, { 1, 1, 2, 3, 0 } };
+            Matrix matrix1 = new Matrix(elements1, field5.q);
+            Matrix matrix2 = new Matrix(elements2, field5.q);
+            expedtedStringOutput = "0 0 3 2 1 \n3 2 4 2 2 \n3 2 0 1 1 \n4 0 2 2 1 \n";
+            
+            Assert.Equal(expedtedStringOutput, (matrix1 - matrix2).ToString());
             
         }
 
         [Fact]
         public void OperatorMinus_CheckIfMatrixSubtractionIsAllowed()
         {
+            Field field2 = new Field(2);
+            
+            int[,] firstElements = { { 1, 0, 1, 1, 1 }, { 1, 0, 0, 0, 0 }, { 0, 1, 1, 1, 1 }, { 1, 1, 1, 0, 1 } };
+            Matrix firstMatrix = new Matrix(firstElements, field2.q);
+
+            int[,] secondElements = { { 1, 2, 3 }, { 3, 4, 4 } };
+            Matrix secondMatrix = new Matrix(secondElements, field2.q);
+            
+            Assert.Throws<ArithmeticException>(() => firstMatrix - secondMatrix);
+            // ------
+            int[,] thirdElements = { { 1, 2, 3 }, { 3, 4, 4 } };
+            Field field3 = new Field(3);
+            Matrix thirdMatrix = new Matrix(thirdElements, field3.q);
+            
+            Assert.Throws<InvalidOperationException>(() => secondMatrix - thirdMatrix);
+            
             
         }
 
@@ -134,13 +169,49 @@ namespace tests
         [Fact]
         public void OperatorMultiplication_CheckIfMatricesAreMultipliedCorrectly()
         {
+            Field field2 = new Field(2);
+            int[,] firstElements = { { 1, 0, 1, 1, 1 }, { 1, 0, 0, 0, 0 }, { 0, 1, 1, 1, 1 }, { 1, 1, 1, 0, 1 } };
+            Matrix firstMatrix = new Matrix(firstElements, field2.q);
+            int[,] secondElements = { { 1, 1, 0, 0 }, { 0, 1, 0, 0 }, { 0, 1, 1, 1 }, { 1, 0, 0, 0 }, { 1, 0, 1, 1 } };
+            Matrix secondMatrix = new Matrix(secondElements, field2.q);
+            string expedtedStringOutput = "1 0 0 0 \n1 1 0 0 \n0 0 0 0 \n0 1 0 0 \n";
             
+            Assert.Equal(expedtedStringOutput, (firstMatrix * secondMatrix).ToString());
+            
+            // ---
+            
+            Field field5 = new Field(5);
+            int[,] elements1 = { { 1, 0, 2, 3, 4 }, { 1, 1, 0, 3, 2 }, { 3, 1, 1, 2, 1 }, { 0, 1, 4, 0, 1 }, { 2, 3, 1, 1, 1 } };
+            int[,] elements2 = { { 1, 0, 4, 1, 3 }, { 3, 4, 1, 1, 0}, { 0, 4, 1, 1, 0 }, { 1, 1, 2, 3, 0 }, { 4, 3, 2, 1, 0 } };
+            Matrix matrix1 = new Matrix(elements1, field5.q);
+            Matrix matrix2 = new Matrix(elements2, field5.q);
+            expedtedStringOutput = "0 3 0 1 3 \n0 3 0 3 3 \n2 3 0 2 4 \n2 3 2 1 0 \n1 0 1 0 1 \n";
+            
+            Assert.Equal(expedtedStringOutput, (matrix1 * matrix2).ToString());
         }
 
 
         [Fact]
         public void OperatorMultiplication_CheckIfMatrixMultiplicationIsAllowed()
         {
+            Field field2 = new Field(2);
+            
+            int[,] firstElements = { { 1, 0, 1, 1, 1 }, { 1, 0, 0, 0, 0 }, { 0, 1, 1, 1, 1 }, { 1, 1, 1, 0, 1 } };
+            Matrix firstMatrix = new Matrix(firstElements, field2.q);
+
+            int[,] secondElements = { { 1, 2, 3 }, { 3, 4, 4 }, { 3, 3, 3 } };
+            Matrix secondMatrix = new Matrix(secondElements, field2.q);
+            
+            Assert.Throws<ArithmeticException>(() => firstMatrix * secondMatrix);
+            
+            // ------
+            int[,] thirdElements = { { 1, 2, 3 }, { 3, 4, 4 }, { 3, 3, 3 } };
+            Field field3 = new Field(3);
+            Matrix thirdMatrix = new Matrix(thirdElements, field3.q);
+            
+            Assert.Throws<InvalidOperationException>(() => secondMatrix * thirdMatrix);
+            
+            
             
         }
         
