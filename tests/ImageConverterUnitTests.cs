338c4d142d1fe0dcc04960175df10f6efd8ce1da
Elvinas Švilpa <elvinas.svilpa@mif.stud.vu.lt>
2024-10-13 14:29:12 +0300

    Fixed unit test to clean up files

--- tests/ImageConverterUnitTests.cs
+++ tests/ImageConverterUnitTests.cs
@@ -8,7 +8,7 @@ using app.Algorithms;
 // 2min 13s - 2min 30sec without any optimizations
 // 41.284s after first optimizations 
 // 15.693s after second optimization
-// 5 - 10s after third optimization
+// 7 - 13s after third and more optimizations
 public class ImageConverterUnitTests
 {
     [Fact]
@@ -16,8 +16,8 @@ public class ImageConverterUnitTests
     {
         string imagePath = "../../../test-images/1.bmp";
         string binaryPath = "../../../test-images/1.bin"; 
-        string expectedBinaryPath = "../../../test-images/1_expected.bin"; 
-        string savePath = "../../../test-images/1_expected.bmp";
+        string reconvertedBinaryPath = "../../../test-images/1_reconverted.bin"; 
+        string savePath = "../../../test-images/1_reconverted.bmp";
         Image image = Image.Load(imagePath);
         Stopwatch stopwatch = Stopwatch.StartNew();
         Matrix convertedImage = ImageConverter.ConvertToBinaryMatrix(image, binaryPath);
@@ -26,10 +26,25 @@ public class ImageConverterUnitTests
         stopwatch.Reset();
         
         stopwatch.Start();
-        ImageConverter.SaveImage(convertedImage, expectedBinaryPath, savePath);
+        ImageConverter.SaveImage(convertedImage, reconvertedBinaryPath, savePath);
         stopwatch.Stop();
         Console.WriteLine($"Saving image took {stopwatch.ElapsedMilliseconds} ms");
+
+        byte[] convertedBinary = File.ReadAllBytes(binaryPath);
+        byte[] reconvertedBinary = File.ReadAllBytes(reconvertedBinaryPath);
+        Assert.True(convertedBinary.SequenceEqual(reconvertedBinary), "Converted binary file is not the same as the original one." );
         
+        if (File.Exists(binaryPath))
+        {
+            File.Delete(binaryPath);
+        }
+
+        if (File.Exists(reconvertedBinaryPath))
+        {
+            File.Delete(reconvertedBinaryPath);
+        }
+        
+
     }
     
     
