8dc4e61a2256690e1c453f60d9d3971f769b1e5a
Elvinas Švilpa <elvinas.svilpa@mif.stud.vu.lt>
2024-12-10 02:35:15 +0200

    documented RandomNumberGenerator.cs and StandardArrayGenerator.cs

--- app/Math/RandomNumberGenerator.cs
+++ app/Math/RandomNumberGenerator.cs
@@ -1,18 +1,27 @@
 namespace app.Math;
 
 
-
+/// <summary>
+/// This class implements the <c>INumGen</c> interface to generate random numbers.
+/// </summary>
 public class RandomNumberGenerator : INumGen
 {
     private double RandomNumber;
     private Random random;
 
+    /// <summary>
+    /// Constructor which generates a random number stored in <see cref="random"/>
+    /// </summary>
     public RandomNumberGenerator()
     {
         this.random = new Random();
     }
-
-    // this method is virtual, so that later on in the mock tests it is able to be overriden
+    
+    /// <summary>
+    /// Method to generate a number.
+    /// <remarks>This method is virtual, so that later on in the mock tests it is able to be overriden.</remarks>
+    /// </summary>
+    /// <returns></returns>
     public virtual double GenerateNumber()
     {
         return random.NextDouble();
