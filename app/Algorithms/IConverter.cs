35ae1e6b2b78c851f0a5a32d69d34e357e6928a4
Elvinas Švilpa <elvinas.svilpa@mif.stud.vu.lt>
2024-12-10 02:48:07 +0200

    additional documentation

--- app/Algorithms/IConverter.cs
+++ app/Algorithms/IConverter.cs
@@ -5,12 +5,21 @@ namespace app.Algorithms;
 
 using app.Math;
 
+/// <summary>
+/// Interface for all converter algorithms.
+/// </summary>
+/// <typeparam name="T">Generic type which specifies the type of converter created.</typeparam>
 public interface IConverter<T>
 {
     static abstract byte[] ConvertToBinaryArray(T input, string path);
     static abstract T ConvertToOriginalFormat(byte[] input, string path);
     
 
+    /// <summary>
+    /// Static method from this interface to make a matrix from a byte array. Useful while image encoding/decoding.
+    /// </summary>
+    /// <param name="array">byte array</param>
+    /// <returns><c>Matrix</c></returns>
     static Matrix MakeMatrixFromByteArray(byte[] array)
     {
         int[,] binaryVector = new int[1, array.Length * 8]; // * 8 since each byte is 8 bits long
@@ -36,6 +45,12 @@ public interface IConverter<T>
         return binaryMatrix;
     }
 
+    /// <summary>
+    /// Method to make a matrix from a byte file. No longer required for decoding, search for alternatives in decoding algorithms.
+    /// </summary>
+    /// <param name="filePath">File path from which a matrix will be made.</param>
+    /// <returns><c>Matrix</c></returns>
+    [Obsolete("No longer required for decoding, check decoder algorithms.")]
     static Matrix MakeMatrixFromByteFile(string filePath)
     {
         byte[] bytes = File.ReadAllBytes(filePath);
@@ -57,6 +72,11 @@ public interface IConverter<T>
     
     // previously 1.654s
     // works only on vectors 
+    /// <summary>
+    /// Method to make a byte array from a specified matrix. Useful for image encoding/decoding.
+    /// </summary>
+    /// <param name="matrix">Matrix, which will be made into a byte array.</param>
+    /// <returns><c>byte[]</c></returns>
     static byte[] MakeByteArrayFromMatrix(Matrix matrix)
     {
         int numberOfBytes = (int)System.Math.Ceiling(matrix.Columns / 8.0);
@@ -87,6 +107,11 @@ public interface IConverter<T>
         return bytes;
     }
     
+    /// <summary>
+    /// Makes a matrix from a bit array. Used in <see cref="UpdatedLinearEncodingAlgorithm"/> for image encoding/decoding.
+    /// </summary>
+    /// <param name="bitArray">Bit array.</param>
+    /// <returns><c>Matrix</c></returns>
     public static Matrix MakeMatrixFromBitArray(BitArray bitArray)
     {
         int[,] binaryVector = new int[1, bitArray.Length];
